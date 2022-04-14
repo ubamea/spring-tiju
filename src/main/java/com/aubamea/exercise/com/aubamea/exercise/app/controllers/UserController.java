321601fb91e70dd1f3410c9c2ce2c90171901c38
alberto ubamea <albertoubamea@gmail.com>
2022-04-14 15:27:20 -0700

    addCors

--- src/main/java/com/aubamea/exercise/com/aubamea/exercise/app/controllers/UserController.java
+++ src/main/java/com/aubamea/exercise/com/aubamea/exercise/app/controllers/UserController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 @Validated
 @RequiredArgsConstructor
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/user")
 public class UserController {
 
